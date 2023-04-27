609e26e57813a70830ccb934861d14d64789c5c6
Carlos Mirabal <carlosmirabal@totpes.com>
2023-04-27 10:45:47 +0200

    packages

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "react-native-screens/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+export default function AppNavigator() {
+    return <div>AppNavigator</div>;
+}
